c524a0694b0e2ac20e934fdd7652f133a09baf86
Taiyou Komazawa <taiyou24690@gmail.com>
2021-08-18 15:57:40 +0900

    [fix] extern宣言のし忘れを修正

--- src/cobs.hpp
+++ src/cobs.hpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-size_t cobsEncode(const void *data, size_t length, uint8_t *buffer);
-size_t cobsDecode(const uint8_t *buffer, size_t length, void *data);
+extern size_t cobsEncode(const void *data, size_t length, uint8_t *buffer);
+extern size_t cobsDecode(const uint8_t *buffer, size_t length, void *data);
 
 #endif
