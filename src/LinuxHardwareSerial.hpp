9e1a0b5d546a55750ec5d7bc9366b1c0debe8281
Taiyou Komazawa <taiyou24690@gmail.com>
2021-08-18 16:14:16 +0900

    [update] ヘッダー依存部分を移行

--- src/LinuxHardwareSerial.hpp
+++ src/LinuxHardwareSerial.hpp
@@ -14,74 +14,22 @@
 
 #include "SerialDev.hpp"
 
-extern "C"
-{
-    int fdwrite(int fd, unsigned char *data, unsigned int len)
-    {
-        return write(fd, data, len);
-    }
-
-    int fdread(int fd)
-    {
-        unsigned char c[1];
-        if(read(fd, c, 1) == 1)
-            return *c;
-        else
-            return 0;
-    }
-
-    long fdtell(int fd)
-    {
-        return lseek(fd, 0, SEEK_CUR);
-    }
-}
 
 class LinuxHardwareSerial : public SerialDev
 {
 public:
-    LinuxHardwareSerial(const char port[], int baud_rate = B9600)
-    {
-        int fd = open(port, O_RDWR);
-        if (fd < 0){
-            perror("open port failed.");
-            exit(EXIT_FAILURE);
-        }
-
-        struct termios tio;
-        tio.c_cflag += CREAD;  //rx eable
-        tio.c_cflag += CLOCAL; //local
-        tio.c_cflag += CS8;    //8bit
-
-        cfsetispeed(&tio, baud_rate);
-        cfsetospeed(&tio, baud_rate);
-
-        cfmakeraw(&tio); //raw mode
-
-        tcsetattr(fd, TCSANOW, &tio);
-        ioctl(fd, TCSETS, &tio);
-
-        _fd = fd;
-    }
+    LinuxHardwareSerial(const char port[], int baud_rate = B9600);
 
     ~LinuxHardwareSerial()
     {
         close(_fd);
     }
 
-    virtual int read()
-    {
-        return fdread(_fd);
-    }
+    virtual int read();
 
-    virtual int readable_len()
-    {
-        return (int)fdtell(_fd);
-    }
+    virtual int readable_len();
 
-    virtual int write(unsigned char *data, unsigned int len)
-    {
-        return fdwrite(_fd, data, len);
-    }
+    virtual int write(unsigned char *data, unsigned int len);
 
     private:
         int _fd;
--- src/source/LinuxHardwareSerial.cpp
+++ src/source/LinuxHardwareSerial.cpp
@@ -1,1 +1,63 @@
 #include "../LinuxHardwareSerial.hpp"
+
+extern "C"
+{
+    int fdwrite(int fd, unsigned char *data, unsigned int len)
+    {
+        return write(fd, data, len);
+    }
+
+    int fdread(int fd)
+    {
+        unsigned char c[1];
+        if(read(fd, c, 1) == 1)
+            return *c;
+        else
+            return 0;
+    }
+
+    long fdtell(int fd)
+    {
+        return lseek(fd, 0, SEEK_CUR);
+    }
+}
+
+LinuxHardwareSerial::LinuxHardwareSerial(const char port[], int baud_rate)
+{
+    int fd = open(port, O_RDWR);
+    if (fd < 0)
+    {
+        perror("open port failed.");
+        exit(EXIT_FAILURE);
+    }
+
+    struct termios tio;
+    tio.c_cflag += CREAD;  //rx eable
+    tio.c_cflag += CLOCAL; //local
+    tio.c_cflag += CS8;    //8bit
+
+    cfsetispeed(&tio, baud_rate);
+    cfsetospeed(&tio, baud_rate);
+
+    cfmakeraw(&tio); //raw mode
+
+    tcsetattr(fd, TCSANOW, &tio);
+    ioctl(fd, TCSETS, &tio);
+
+    _fd = fd;
+}
+
+int LinuxHardwareSerial::read()
+{
+    return fdread(_fd);
+}
+
+int LinuxHardwareSerial::readable_len()
+{
+    return (int)fdtell(_fd);
+}
+
+int LinuxHardwareSerial::write(unsigned char *data, unsigned int len)
+{
+    return fdwrite(_fd, data, len);
+}
