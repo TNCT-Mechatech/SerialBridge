9e90796a6bc466ea60712a3a35238fcfeaa3e80b
Taiyou Komazawa <taiyou24690@gmail.com>
2021-08-23 16:17:37 +0900

    [fix/WIP] パケットの検出部分の判定処理の修正
    パケットデータ前後に0をつけ、それを検出する処理に変更。
    TODO: Arduino環境でのテスト、Linux環境だとなぜか途中でうまくパケットを切り出せなくなる問題の修正。

--- src/source/CobsSerial.cpp
+++ src/source/CobsSerial.cpp
@@ -14,42 +14,60 @@ int CobsSerial::read(uint8_t *data, const unsigned int max_len)
     int cnt = 0;
     uint8_t tmp[max_len] = {};
 
-    if (_got_packet)
-    {
-        uint8_t val = 0;
-        while (_rx_buffer.size())
-        {
+    if(!_data_begin && _got_packet){
+        uint8_t val = 1;
+        while (_rx_buffer.size()){
             val = _rx_buffer.front();
             _rx_buffer.pop();
             tmp[cnt] = val;
             cnt++;
-            if (val == 0)
-            { //次のデータパケットの終点で終了
-                _got_packet = false;
-                cnt = cobsDecode(tmp, cnt, data);
-                break;
-            }
+        }
+
+        if (val == 0){ //データパケットの終端で終了
+            _got_packet = false;
+            cnt = cobsDecode(tmp, cnt, data);
+        }else{
+            cnt = 0;
         }
     }
-
     return cnt;
 }
 
 int CobsSerial::write(uint8_t *data, const unsigned int len)
 {
     uint8_t tmp[COBS_TX_BUFFER_SIZE] = {};
-    int tx_size = cobsEncode(data, len, tmp);
-    return _dev->write(data, tx_size);
+    int tx_size = cobsEncode(data, len, tmp+1); //先頭の次にcobsコードを代入
+    return _dev->write(tmp, tx_size+2);
 }
 
 void CobsSerial::update()
 {
     if(_dev->readable_len()){
         char tmp = _dev->read();
-        if(tmp == 0 && _rx_buffer.size() && !_got_packet){ //データの終端
-            _rx_buffer.push(tmp);
-            _got_packet = true;
-        }else if (_rx_buffer.size()){ //データの追加
+
+        if(_data_begin && _got_packet){
+            if(tmp){                    //データの始端(始端の次のデータが始端以外であるとき)
+                _rx_buffer.push(tmp);
+                _got_packet = false;
+                return;
+            }
+        }
+
+        if(!tmp){                       //データが0であったときの処理
+            if(!_rx_buffer.size()){     //データが入っていないとき
+                _data_begin = true;
+                return;                 //フラグがtrue,trueで終了
+            }else{                      //データの終端(データが入っているとき)
+                _rx_buffer.push(tmp);
+
+                _data_begin = false;
+                _got_packet = true;
+
+                return;
+            }
+        }
+
+        if(_data_begin && !_got_packet){ //データの追加
             _rx_buffer.push(tmp);
         }
     }
