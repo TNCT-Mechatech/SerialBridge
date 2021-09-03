a1775b1688ae39bc1851dad9b4351c589f60a39f
testusuke <silverbro007@icloud.com>
2021-09-03 15:04:37 +0900

    [Update] separate header and source

--- src/MbedHardwareSerial.hpp
+++ src/MbedHardwareSerial.hpp
@@ -14,62 +14,27 @@
 //  for using queue
 #include "CobsSerial.hpp"
 
-#define RX_QUEUE_SIZE 64
-
 /**
 * @brief This class is an implementation for Mbed devices that inherits the SerialDev class.
 */
 class MbedHardwareSerial : public SerialDev
 {
 public:
-    /**
-    * @brief MbedHardwareSerial class constructor.
-    * @param[in] dev (Serial class pointer)A class object for doing character / binary
-    * @par Example
-    *   When using USBTX/RX pin, declare as follows.
-    *
-    *   Serial serial(USBTX, USBRX);
-    *   MbedHardwareSerial obj(&serial);
-    *
-    */
-    MbedHardwareSerial(Serial *dev) : _rx_queue(RX_QUEUE_SIZE)
-    {
-        _dev = dev;
-        _dev->attach(callback(this, &MbedHardwareSerial::_serial_rx), Serial::RxIrq);
-    }
+    MbedHardwareSerial(Serial *dev);
 
-    virtual int read()
-    {
-        uint8_t data = _rx_queue.front();
-        _rx_queue.pop();
-        return data;
-    }
+    virtual int read();
 
-    virtual int readable_len()
-    {
-        return _rx_queue.size();
-    }
+    virtual int readable_len();
 
-    virtual int write(unsigned char *data, unsigned int len)
-    {
-        for(int i = 0; i < len; i++){
-            _dev->putc(data[i]);
-        }
-        return len;
-    }
+    virtual int write(unsigned char *data, unsigned int len);
+
+protected:
+    enum{
+        RX_QUEUE_SIZE = 64,
+    };
 
 private:
-    /**
-     * @brief This function is called when raise rx event
-     */
-    void _serial_rx(void)
-    {
-        while (_dev->readable()) {
-            unsigned char c = _dev->getc();
-            //  append character into queue
-            _rx_queue.push(c);
-        }
-    }
+    void _serial_rx(void);
 
     Serial *_dev;
     //  rx character queue
---
+++ src/source/MbedHardwareSerial.cpp
@@ -0,0 +1,70 @@
+/**
+* @file MbedHardwareSerial.hpp
+* @brief Define the class required for serial communication using SerialBridge in Mbed environment.
+* @author testusuke
+* @date 2021/9/3
+*/
+
+#include "../MbedHardwareSerial.hpp"
+
+/**
+* @brief MbedHardwareSerial class constructor.
+* @param[in] dev (Serial class pointer)A class object for doing character / binary
+* @par Example
+*   When using USBTX/RX pin, declare as follows.
+*
+*   Serial serial(USBTX, USBRX);
+*   MbedHardwareSerial obj(&serial);
+*/
+MbedHardwareSerial::MbedHardwareSerial(Serial *dev) 
+: _rx_queue(RX_QUEUE_SIZE)
+{
+    _dev = dev;
+    _dev->attach(callback(this, &MbedHardwareSerial::_serial_rx), Serial::RxIrq);
+}
+
+/**
+* @brief Return a received character
+* @return uint8_t 
+*/
+virtual int MbedHardwareSerial::read()
+{
+    uint8_t data = _rx_queue.front();
+    _rx_queue.pop();
+    return data;
+}
+
+/**
+* @brief Return number of available characters
+* @return int of available chracter.
+*/
+virtual int MbedHardwareSerial::readable_len()
+{
+    return _rx_queue.size();    
+}
+
+/**
+* @brief A function of write array of charaters
+* @param[in] data (characters pointer) characters
+* @param[in] len (int) length of characters 
+* @return int length of characters
+*/ 
+virtual int MbedHardwareSerial::write(unsigned char *data, unsigned int len)
+{
+    for(int i = 0; i < len; i++){
+        _dev->putc(data[i]);
+    }
+    return len;
+}
+
+/**
+* @brief A function is called when raise rx event
+*/ 
+void MbedHardwareSerial::_serial_rx(void)
+{
+    while (_dev->readable()) {
+        unsigned char c = _dev->getc();
+        //  append character into queue
+        _rx_queue.push(c);
+    }
+}
