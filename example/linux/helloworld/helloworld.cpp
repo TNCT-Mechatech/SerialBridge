c082f62e744afb7915b2100be7df3f4200974be9
taiyou komazawa <taiyou24690@gmail.com>
2021-09-05 18:30:55 +0900

    [doc] ファイル内に説明文を追加。

--- example/linux/helloworld/helloworld.cpp
+++ example/linux/helloworld/helloworld.cpp
@@ -1,5 +1,20 @@
-//g++ -Wall -Wextra  helloworld.cpp ../../../bin/libSerialBridge.a -I ../../../src
+/**
+* @file helloworld.cpp
+* @brief A simple program example for communicating strings.
+* @author Taiyou Komazawa
+* @date 2021/9/5
+* @details This is an example of using "Serial Bridge" in a linux environment.
+* Change "SERIAL_PATH" according to the path of the virtual file to which the microcomputer is connected.
+* When compiling, execute "make all" in the "SerialBridge" directory,
+*  and then execute the following command in the directory where this file is located.
+*
+* g++ -Wall -Wextra  helloworld.cpp ../../../bin/libSerialBridge.a -I ../../../src
+*
+* It works by executing the generated file "a.out". (Command:"./a.out")
+*
+*/
 
+//linux os identification macro.
 #ifdef __linux__
 
 #include <stdio.h>
@@ -11,30 +26,41 @@
 
 #define SERIAL_PATH "/dev/ttyUSB0"
 
+//Define a structure with a 32 bytes character string.
 typedef struct{
     char c[32];
 } words_t;
 
+//Name the sb::Message type,which has a data structure of the words_t type,the Words type.
 typedef sb::Message<words_t> Words;
 
 Words hello;
 
+//Pass the serial derived class entity for Linux to SerialDev.
 SerialDev *dev = new LinuxHardwareSerial(SERIAL_PATH, B9600);
 SerialBridge serial(dev);
 
 int main()
 {
+    //Register "hello" message with ID=123.
     serial.add_frame(123, &hello);
+    //Stores a string inside the structure of the "hello" message.(I used sprintf.)
     sprintf(hello.data.c, "Hello world from linux.\n");
 
+    //Send a message with ID = 123 ("hello" message) once.
     serial.write(123);
 
-    while(1){
+    while(1){   //Data reception loop.
+        //Get the message. (If there is an update, the return value is 0)
         bool read_success = serial.read() == 0;
-        if (read_success){
+
+        if (read_success){  //If read () succeeds.
+            //If the other party writes the same data structure to ID = 123
+            //"hello.data.c" will be updated with the string sent by the other party.
             printf("%s", hello.data.c);
         }
 
+        //Get data from the serial bus.
         serial.update();
     }
 }
--- example/mbed/helloworld/helloworld.cpp
+++ example/mbed/helloworld/helloworld.cpp
@@ -1,5 +1,17 @@
+/**
+* @file helloworld.cpp
+* @brief A simple program example for communicating strings.
+* @author Taiyou Komazawa
+* @date 2021/9/5
+* @details This is an example of using SerialBridge in the mbed environment.
+*  Please enable "#define ENABLE_HELLO_EXAMPLE_MAIN" which is commented out to prevent
+*  overloading of "main()" before compiling.
+* @note Operation confirmed with mbed-os v6.14.0.
+*/
+
 //#define ENABLE_HELLO_EXAMPLE_MAIN
 
+//ARM-GCC compiler identification macro.
 #if defined(__arm__) && defined(ENABLE_HELLO_EXAMPLE_MAIN)
 
 #include "mbed.h"
@@ -10,37 +22,49 @@
 
 #include <Message.hpp>
 
-typedef struct
-{
+//Define a structure with a 32 bytes character string.
+typedef struct{
     char c[32];
 } words_t;
 
+//Name the sb::Message type,which has a data structure of the words_t type,the Words type.
 typedef sb::Message<words_t> Words;
 
 Words hello;
 
+//Pass the serial derived class entity for Arduino to SerialDev.
 SerialDev *dev = new MbedHardwareSerial(new BufferedSerial(USBTX, USBRX, 9600));
 SerialBridge serial(dev);
 
+//Enable built-in LED lamp for result display.
 DigitalOut myled(LED1);
 
 int main()
 {
+    //Register "hello" message with ID=123.
     serial.add_frame(123, &hello);
+    //Stores a string inside the structure of the "hello" message.(I used sprintf.)
     sprintf(hello.data.c, "Hello world from mbed.\n");
 
+    //Send a message with ID = 123("hello" message) once.
     serial.write(123);
 
+    //LED lamp is off.
     myled = 0;
 
-    while (1)
-    {
+    while(1){   //Data reception loop.
+        //Get the message. (If there is an update, the return value is 0)
         bool read_success = serial.read() == 0;
-        if (read_success)
-        {
+
+        if(read_success){  //If read () succeeds.
+            //If the other party writes the same data structure to ID = 123
+            //"hello.data.c" will be updated with the string sent by the other party.
+            //However,since there is only one serial bus,the built-in LED lamp is lit to display the result.
+            //(If you can monitor the LCD, other Serial ports, etc., try changing the process here.)
             myled = 1;
         }
 
+        //Get data from the serial bus.
         serial.update();
     }
 }
