7b556c6213100a65bb2f5e283a6d65f858efedfb
testusuke <silverbro007@icloud.com>
2023-06-27 12:18:43 +0900

    docs: add comments to SerialBridge.cpp

--- src/source/SerialBridge.cpp
+++ src/source/SerialBridge.cpp
@@ -13,16 +13,14 @@
 * @param[in] buff_size Receive buffer size.(bytes)
 */
 SerialBridge::SerialBridge(SerialDev *dev, const unsigned int buff_size)
-:   _id_list(), _buff_size(buff_size)
-{
+        : _id_list(), _buff_size(buff_size) {
     _dev = new CobsSerial(dev, _buff_size);
 }
 
 /**
 * @brief SerialBridge class destructor.
 */
-SerialBridge::~SerialBridge()
-{
+SerialBridge::~SerialBridge() {
     delete _dev;
 }
 
@@ -37,25 +35,29 @@ SerialBridge::~SerialBridge()
 * @retval -1 : Failure. The argument str specified was NULL.
 * @retval -2 : Failure. The number of registered message frames has reached the upper limit.
 */
-int SerialBridge::add_frame(SerialBridge::frame_id id, sb::MessageInterface *str)
-{
-    if(str == NULL)
+int SerialBridge::add_frame(SerialBridge::frame_id id, sb::MessageInterface *str) {
+    if (str == NULL) {
         return -1;
+    }
 
+    //  check if structure has already registered.
     int order = _id_2_order(id);
-    if(order >= 0){
+    if (order >= 0) {
+        //  update structure
         _str[order] = str;
         return 0;
     }
 
-    for(int i = 0; i < STRUCT_MAX_NUM; i++){
-        if(_str[i] == NULL){
+    //  find available order and register them.
+    for (int i = 0; i < STRUCT_MAX_NUM; i++) {
+        if (_str[i] == NULL) {
             _str[i] = str;
             _id_list[i] = id;
             return 0;
         }
     }
 
+    //  failed to add message due to fail to find available order.
     return -2;
 }
 
@@ -67,16 +69,19 @@ int SerialBridge::add_frame(SerialBridge::frame_id id, sb::MessageInterface *str
 * @retval  0 : Success.
 * @retval -1 : Unregistered message id.
 */
-int SerialBridge::rm_frame(frame_id id)
-{
+int SerialBridge::rm_frame(frame_id id) {
+    //  find order by frame id
     int order = _id_2_order(id);
 
-    if(order < 0)
-        return -1; //undefined data structure
+    //  check if structure not exists
+    if (order < 0) {
+        return -1;
+    }
 
-    for(int i = order; _str[i] != NULL; i++){
-        _str[i] = _str[i+1];
-        _id_list[i] = _id_list[i+1];
+    //  swap elements which located after removed structure.
+    for (int i = order; _str[i] != NULL; i++) {
+        _str[i] = _str[i + 1];
+        _id_list[i] = _id_list[i + 1];
     }
 
     return 0;
@@ -88,14 +93,18 @@ int SerialBridge::rm_frame(frame_id id)
 * @return int Number of written data(byte) or error code.
 * @retval -1 : Unregistered message id.
 */
-int SerialBridge::write(SerialBridge::frame_id id)
-{
+int SerialBridge::write(SerialBridge::frame_id id) {
     int order = _id_2_order(id);
 
-    if(order < 0)
-        return -1; //undefined data structure
+    //  check if structure not exists
+    if (order < 0) {
+        return -1;
+    }
 
+    //  pack message with frame_id
     _str[order]->packing(id);
+
+    //  write message
     return _dev->write(_str[order]->ptr(), _str[order]->size());
 }
 
@@ -109,9 +118,11 @@ int SerialBridge::write(SerialBridge::frame_id id)
 * @retval -2 : Received packet is invalid.
 * @retval -3 : The id of the received message is unregistered.
 */
-int SerialBridge::update()
-{
+int SerialBridge::update() {
+    //  update device buffer
     _dev->update();
+
+    //  update frame
     return _update_frame();
 }
 
@@ -121,11 +132,11 @@ int SerialBridge::update()
 * @return int Number of elements or error.
 * @retval -1 : Its id is not included in the array.
 */
-int SerialBridge::_id_2_order(frame_id id)
-{
-    for(int i = 0; _str[i] != NULL; i++){
-        if(id == _id_list[i])
+int SerialBridge::_id_2_order(frame_id id) {
+    for (int i = 0; _str[i] != NULL; i++) {
+        if (id == _id_list[i]) {
             return i;
+        }
     }
     return -1;
 }
@@ -140,29 +151,47 @@ int SerialBridge::_id_2_order(frame_id id)
 * @retval -2 : Received packet is invalid.
 * @retval -3 : The id of the received message is unregistered.
 */
-int SerialBridge::_update_frame()
-{
+int SerialBridge::_update_frame() {
+    //  prepare buffer to convert message
     uint8_t tmp[_buff_size];
+    //  fill zero
     memset(&tmp, 0, _buff_size);
+
+    //  read message data from device
     int len = _dev->read(tmp) - 1;
-    if(len > 0){
+
+    if (len > 0) {
+        //  get order by frame_id
         int order = _id_2_order(tmp[0]);
 
-        if(order < 0)
-            return -3; //undefined data structure
+        if (order < 0) {
+            //  failed to find message
+            return -3;
+        }
 
+        //  summary of data for check sum
         uint32_t sum = 0;
-        for(int i = 0; i < len-1; i++)
+        //  add all data
+        for (int i = 0; i < len - 1; i++) {
             sum += tmp[i];
+        }
 
-        if(tmp[len - 1] == (uint8_t)(sum & 0xFF) && len == _str[order]->size()){
-            for (int i = 0; i < len; i++)
+        //  check summary of data, message data length
+        if (tmp[len - 1] == (uint8_t) (sum & 0xFF) && len == _str[order]->size()) {
+            //  insert data to message
+            for (int i = 0; i < len; i++) {
                 _str[order]->ptr()[i] = tmp[i];
+            }
+
+            //  unpack message
             _str[order]->unpacking();
             return 0;
-        }else
-            return -2; //data error
+        } else {
+            //  failed to valid message
+            return -2;
+        }
     }
 
-    return -1; //cannot received
+    //  failed to receive message due to there are no enough data length
+    return -1;
 }
